bd5ada1021999d2b262d65c2a5f0623c0db11b2e
Xinghan Li <xinghan.sde@gmail.com>
2025-01-01 20:36:33 -0800

    1-notification routes and controllers added

---
+++ backend/routes/notification.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+    getNotifications,
+    deleteNotifications,
+    // deleteNotification,
+} from "../controllers/notifications.controller.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getNotifications);
+router.delete("/", protectRoute, deleteNotifications);
+// router.delete("/:id", protectRoute, deleteNotification);
+
+export default router;
